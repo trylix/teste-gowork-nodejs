65c944c830cc1c4cca6111a7d9efdf7fb3cb30fd
Brendenson Andrade <brendensond@gmail.com>
2020-05-20 10:08:41 -0300

    integration test: Registro e listagem de unidades

--- __tests__/factories.js
+++ __tests__/factories.js
@@ -1,12 +1,20 @@
 import { factory } from 'factory-girl';
 import faker from 'faker';
 
+import Office from '../src/app/models/Office';
 import User from '../src/app/models/User';
 
 factory.define('User', User, {
   name: faker.name.findName(),
   email: faker.internet.email(),
   password: faker.internet.password(),
+});
+
+factory.define('Office', Office, {
+  name: `Escritório ${faker.commerce.productName()}`,
+  city: faker.address.city(),
+  state: faker.address.state(),
+  image: 'images/noimage.png',
 });
 
 export default factory;
---
+++ __tests__/integration/office.test.js
@@ -0,0 +1,84 @@
+import bcrypt from 'bcryptjs';
+import request from 'supertest';
+
+import app from '../../src/App';
+import factory from '../factories';
+import truncate from '../util/truncate';
+
+describe('Office', () => {
+  let user;
+  let token;
+
+  beforeEach(async () => {
+    await truncate();
+
+    user = await factory.create('User', {
+      password: bcrypt.hashSync('123456', 8),
+    });
+
+    const response = await request(app).post('/api/auth').send({
+      email: user.email,
+      password: '123456',
+    });
+
+    const { access_token } = response.body;
+
+    token = access_token;
+  });
+
+  it('should be able to register office', async () => {
+    const otherOffice = await factory.attrs('Office');
+
+    const response = await request(app)
+      .post('/api/offices')
+      .set('Authorization', `Bearer ${token}`)
+      .send(otherOffice);
+
+    expect(response.status).toBe(201);
+
+    expect(response.body).toHaveProperty('id');
+  });
+
+  it('should not be able register office with duplicated info', async () => {
+    const office = await factory.create('Office');
+
+    const { name, city, state, image } = office;
+
+    const response = await request(app)
+      .post('/api/offices')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ name, city, state, image });
+
+    expect(response.status).toBe(401);
+  });
+
+  it('should not be able register office missing field', async () => {
+    const { name, city, state } = await factory.attrs('Office');
+
+    const response = await request(app)
+      .post('/api/offices')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        name,
+        city,
+        state,
+      });
+      
+    expect(response.status).toBe(401);
+  });
+
+  it('should be able list all offices', async () => {
+    const response = await request(app)
+      .get('/api/offices')
+      .set('Authorization', `Bearer ${token}`)
+      .send();
+
+    expect(response.status).toBe(200);
+  });
+
+  it('should not be able list offices missing authorization', async () => {
+    const response = await request(app).get('/api/offices').send();
+
+    expect(response.status).toBe(401);
+  });
+});
